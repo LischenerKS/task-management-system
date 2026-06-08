bc70b5f25c596cc6fedd0cea89d29d78f6d81454
lischenerks <lischenerks@gmail.com>
2026-06-08 13:53:06 +0300

    init

---
+++ src/test/java/io/github/lischenerks/taskmanagement/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lischenerks.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

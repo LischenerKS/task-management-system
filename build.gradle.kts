c26582350bbf6a10ac247cd982c0b1c0d7b6ef76
lischenerks <lischenerks@gmail.com>
2026-07-10 11:42:51 +0300

    add spring-boot-starter-actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     runtimeOnly("org.postgresql:postgresql")
 
